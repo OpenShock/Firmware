3406c76eaabdf94ba523690cacc456fda18457c8
Red Mushie <github@redmushie.me>
2023-09-20 08:48:40 +0200

    Include string literal quotes in generated macros

--- embed_firmware_version.py
+++ embed_firmware_version.py
@@ -2,9 +2,9 @@
 Import("env")
 
 env.Append(CPPDEFINES=[
-    ("SHOCKLINK_API_URL", "api.shocklink.net"),
-    ("SHOCKLINK_DEV_API_URL", "dev-api.shocklink.net"),
-    ("SHOCKLINK_FW_VERSION", "0.8.1")
+    ("SHOCKLINK_API_URL", "\"api.shocklink.net\""),
+    ("SHOCKLINK_DEV_API_URL", "\"dev-api.shocklink.net\""),
+    ("SHOCKLINK_FW_VERSION", "\"0.8.1\"")
 ])
 
 print(env.Dump())
