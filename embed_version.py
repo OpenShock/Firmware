278947d0c05e2829d5e2e111081a263db398b51d
Red Mushie <github@redmushie.me>
2023-09-20 09:25:36 +0200

    Remove excess newline

--- embed_version.py
+++ embed_version.py
@@ -5,7 +5,6 @@ Import("env")
 # This file is invoked by PlatformIO during build.
 # See "extra_scripts" in "platformio.ini".
 
-
 # Fetch environment variables, with sane defaults if not found.
 # These variables should be defined by the CI.
 shocklinkApiUrl = os.getenv('SHOCKLINK_API_URL') or "api.shocklink.net"
