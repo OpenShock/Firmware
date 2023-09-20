b4a37d6317f9e7a6cb1bd13bb5c25333b500ad90
Red Mushie <github@redmushie.me>
2023-09-20 09:02:55 +0200

    Add comments to embed_version.py

--- embed_version.py
+++ embed_version.py
@@ -2,10 +2,19 @@ import os
 
 Import("env")
 
+# This file is invoked by PlatformIO during build.
+# See "extra_scripts" in "platformio.ini".
+
+
+# Fetch environment variables, with sane defaults if not found.
+# These variables should be defined by the CI.
 shocklinkApiUrl = os.getenv('SHOCKLINK_API_URL') or "api.shocklink.net"
 shocklinkFwVersion = os.getenv('SHOCKLINK_FW_VERSION') or "0.8.1"
 
+# Define these variables as macros for expansion during build time.
 env.Append(CPPDEFINES=[
     ("SHOCKLINK_API_URL", env.StringifyMacro(shocklinkApiUrl)),
     ("SHOCKLINK_FW_VERSION", env.StringifyMacro(shocklinkFwVersion))
 ])
+
+print(env.Dump())
