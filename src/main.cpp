2a5c5df087107e364954f6541a27d9c5893ebbb2
hhvrc <github@heavenvr.tech>
2023-09-25 16:38:29 +0200

    Move SerialHandler out of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "CaptivePortal.h"
 #include "Constants.h"
 #include "FileUtils.h"
+#include "SerialInputHandler.h"
 #include "WiFiManager.h"
 
 #include <esp_log.h>
@@ -15,11 +16,8 @@ const char* const TAG = "ShockLink";
 
 std::unique_ptr<ShockLink::APIConnection> s_apiConnection = nullptr;
 
-bool setupOk = false;
-
 void setup() {
-  Serial.begin(115'200);
-  Serial.setDebugOutput(true);
+  ShockLink::SerialInputHandler::Init();
 
   ESP_LOGI(TAG, "==== ShockLink v%s ====", ShockLink::Constants::Version);
 
@@ -37,90 +35,10 @@ void setup() {
     rmtPinFile.close();
   }
   ESP_LOGD(TAG, "RMT pin is: %d", rmtPin);
-
-  setupOk = true;
-}
-
-String inputBuffer = "";
-
-bool writeCommands(String& command, String& data) {
-  if (command == "authtoken") {
-    return ShockLink::FileUtils::TryWriteFile("/authToken", data);
-  }
-
-  if (command == "rmtpin") {
-    return ShockLink::FileUtils::TryWriteFile("/rmtPin", data);
-  }
-
-  if (command == "networks") {
-    return ShockLink::FileUtils::TryWriteFile("/networks", data);
-  }
-
-  if (command == "debug") {
-    int delimiter     = data.indexOf(' ');
-    String subCommand = data.substring(0, delimiter);
-    subCommand.toLowerCase();
-    if (subCommand != "api") {
-      return false;
-    }
-
-    data = inputBuffer.substring(delimiter + 1);
-
-    File file     = LittleFS.open("/debug/api", FILE_WRITE);
-    uint8_t state = data == "1" ? 1 : 0;
-    file.write(state);
-    file.close();
-
-    Serial.printf("SYS|Success|Dev api state: %d\n", state);
-
-    return true;
-  }
-
-  return false;
-}
-
-void executeCommand() {
-  int delimiter  = inputBuffer.indexOf(' ');
-  String command = inputBuffer.substring(0, delimiter);
-  command.toLowerCase();
-  String data = inputBuffer.substring(delimiter + 1);
-
-  if (data.length() <= 0) {
-    if (command == "restart") {
-      Serial.println("Restarting ESP...");
-      ESP.restart();
-    }
-  }
-
-  if (data.length() > 0) {
-    if (writeCommands(command, data)) {
-      Serial.println("SYS|Success|Command executed");
-      return;
-    }
-  }
-
-  Serial.println("SYS|Error|Command not found or encountered an error");
-}
-
-void handleSerial() {
-  char data = Serial.read();  // Read the incoming data
-  if (data == 0xd) return;
-
-  if (data == 0xa) {
-    Serial.printf("> %s\n", inputBuffer.c_str());
-    executeCommand();
-    inputBuffer = "";
-    return;
-  }
-
-  inputBuffer += data;
 }
 
 void loop() {
-  if (!setupOk) return;
-
-  if (Serial.available()) handleSerial();
-
+  ShockLink::SerialInputHandler::Update();
   ShockLink::CaptivePortal::Update();
 
   if (s_apiConnection != nullptr) {
