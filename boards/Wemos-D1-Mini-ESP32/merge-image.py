12a7e123250e89d51fba59bb06aab258524c4421
Red Mushie <github@redmushie.me>
2023-10-01 15:01:58 +0200

    build: Add per-board merge scripts

---
+++ boards/Wemos-D1-Mini-ESP32/merge-image.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import esptool
+
+# Note: Bootloader for esp32-s3 starts at 0x0000, unlike several other ESP32 variants that start at 0x1000.
+esptool.main([
+    '--chip', 'esp32-s3',
+    'merge_bin', '-o', 'merged.bin',
+    '0x0', './bootloader.bin',
+    '0x8000', './partitions.bin',
+    '0x10000', './firmware.bin',
+    '0x310000', './littlefs.bin'
+])
