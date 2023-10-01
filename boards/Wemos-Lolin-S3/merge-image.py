91dce84048e371bc6ff3d7361a81ed3e7bcafa53
Red Mushie <github@redmushie.me>
2023-10-01 17:06:02 +0200

    build: Add Wemos Lolin S2 and Pishock 2023 boards

--- boards/Wemos-Lolin-S3/merge-image.py
+++ boards/Wemos-Lolin-S3/merge-image.py
@@ -4,7 +4,7 @@ import esptool
 
 # Note: Bootloader for esp32-s3 starts at 0x0000, unlike several other ESP32 variants that start at 0x1000.
 esptool.main([
-    '--chip', 'esp32-s3',
+    '--chip', 'esp32s3',
     'merge_bin', '-o', 'merged.bin',
     '--flash_size', '16MB', # This board has 16MiB of flash.
     '0x0', './bootloader.bin',
