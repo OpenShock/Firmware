6633fe89d0d247885a91b010ef98939516e45b10
hhvrc <github@heavenvr.tech>
2023-10-02 17:41:43 +0200

    Implement better LED selection system

--- src/VisualStateManager.cpp
+++ src/VisualStateManager.cpp
@@ -1,38 +1,30 @@
 #include "VisualStateManager.h"
 
+#ifdef OPENSHOCK_LED_GPIO
+#ifdef OPENSHOCK_LED_IMPLEMENTATION
+#error "Only one LED type can be defined at a time"
+#else
+#define OPENSHOCK_LED_IMPLEMENTATION
 #include "PinPatternManager.h"
-
 #include <Arduino.h>
+#endif // OPENSHOCK_LED_IMPLEMENTATION
+#endif // OPENSHOCK_LED_GPIO
+
+#ifdef OPENSHOCK_LED_WS2812B
+#ifdef OPENSHOCK_LED_IMPLEMENTATION
+#error "Only one LED type can be defined at a time"
+#else
+#define OPENSHOCK_LED_IMPLEMENTATION
+#endif // OPENSHOCK_LED_IMPLEMENTATION
+#endif // OPENSHOCK_LED_WS2812B
+
+#include <esp_log.h>
 
 const char* const TAG = "VisualStateManager";
 
-#ifdef OPENSHOCK_LED_TYPE
-
-#if OPENSHOCK_LED_TYPE == 0
-
-#define OPENSHOCK_LED_TYPE_NONE
-
-#elif OPENSHOCK_LED_TYPE == 1
-
-#ifdef OPENSHOCK_LED_PIN
-#define OPENSHOCK_LED_TYPE_GPIO 1
-#else
-#error "OPENSHOCK_LED_PIN must be defined when OPENSHOCK_LED_TYPE == 1 (GPIO connected LED)"
-#endif
-
-#elif OPENSHOCK_LED_TYPE == 2
-
-#define OPENSHOCK_LED_TYPE_NEOPIXEL
-
-#else
-#error "Unsupported LED type"
-#endif
-
-#else
-#error "OPENSHOCK_LED_TYPE must be defined"
-#endif
-
 using namespace OpenShock;
+
+#ifdef OPENSHOCK_LED_GPIO
 
 constexpr PinPatternManager::State kCriticalErrorPattern[] = {
   { true, 100}, // LED ON for 0.1 seconds
@@ -83,12 +75,9 @@ constexpr PinPatternManager::State kWebSocketConnectedPattern[] = {
   {true, 10'000}
 };
 
-#ifdef OPENSHOCK_LED_TYPE_GPIO
-PinPatternManager s_builtInLedManager(OPENSHOCK_LED_PIN);
-#endif
+PinPatternManager s_builtInLedManager(OPENSHOCK_LED_GPIO);
 
 void VisualStateManager::SetCriticalError() {
-#ifdef OPENSHOCK_LED_TYPE_GPIO
   static bool _state = false;
   if (_state) {
     return;
@@ -97,13 +86,9 @@ void VisualStateManager::SetCriticalError() {
   s_builtInLedManager.SetPattern(kCriticalErrorPattern);
 
   _state = true;
-#else
-  ESP_LOGW(TAG, "SetCriticalError (but LED was not configured at build time)");
-#endif
 }
 
 void VisualStateManager::SetWiFiState(WiFiState state) {
-#ifdef OPENSHOCK_LED_TYPE_GPIO
   static WiFiState _state = (WiFiState)-1;
   if (_state == state) {
     return;
@@ -122,7 +107,30 @@ void VisualStateManager::SetWiFiState(WiFiState state) {
   }
 
   _state = state;
-#else
-  ESP_LOGW(TAG, "SetWiFiStateState: %d (but LED was not configured at build time)", state);
-#endif
 }
+
+#endif // OPENSHOCK_LED_GPIO
+
+#ifdef OPENSHOCK_LED_WS2812B
+
+void VisualStateManager::SetCriticalError() {
+  ESP_LOGE(TAG, "SetCriticalError: (but WS2812B is not implemented yet)");
+}
+
+void VisualStateManager::SetWiFiState(WiFiState state) {
+  ESP_LOGE(TAG, "SetWiFiState: %d (but WS2812B is not implemented yet)", state);
+}
+
+#endif // OPENSHOCK_LED_WS2812B
+
+#ifndef OPENSHOCK_LED_IMPLEMENTATION
+
+void VisualStateManager::SetCriticalError() {
+  ESP_LOGW(TAG, "SetCriticalError: (But no LED implementation is selected)");
+}
+
+void VisualStateManager::SetWiFiState(WiFiState state) {
+  ESP_LOGW(TAG, "SetWiFiState: %d (But no LED implementation is selected)", state);
+}
+
+#endif // OPENSHOCK_LED_NONE
