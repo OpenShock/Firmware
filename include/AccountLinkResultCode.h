96f522305195be5c02c265a60b14769f75c9cbb4
hhvrc <github@heavenvr.tech>
2023-11-01 10:46:04 +0100

    Rename GatewayPair to AccountLink

--- include/AccountLinkResultCode.h
+++ include/AccountLinkResultCode.h
@@ -5,5 +5,5 @@
 #include <cstdint>
 
 namespace OpenShock {
-  typedef OpenShock::Serialization::Local::GatewayPairResultCode GatewayPairResultCode;
+  typedef OpenShock::Serialization::Local::AccountLinkResultCode AccountLinkResultCode;
 }  // namespace OpenShock
