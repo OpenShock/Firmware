16f9529a875f3c0e7773f9448fa6cfdb6576b65d
hhvrc <github@heavenvr.tech>
2023-11-02 15:31:38 +0100

    Fix certain detections

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -2,30 +2,47 @@ import importlib.util
 import sys
 import subprocess
 
-# This function checks if a package is installed using multiple methods
-def is_installed(package):
-    if package in sys.modules:
-        return True
+class Package:
+    def __init__(self, pip_name, pip_options, import_name):
+        self.pip_name = pip_name
+        self.pip_options = pip_options
+        self.import_name = import_name
 
-    if importlib.util.find_spec(package) is not None:
-        return True
+    def pip_name(self):
+        return self.pip_name
 
-    try:
-        __import__(package)
-        return True
-    except ImportError:
-        pass
+    def pip_package(self):
+        if len(self.pip_options) == 0:
+            return self.pip_name
 
-    return False
+        return self.pip_name + '[' + ','.join(self.pip_options) + ']'
 
-# Dictionary of required packages and their pip install names
-required = {
-    'fontTools': 'fonttools[woff,unicode]',
-    'brotli': 'brotli',
-}
+    def import_name(self):
+        return self.import_name
+
+    def is_installed(self):
+        if self.pip_name in sys.modules:
+            return True
+
+        if importlib.util.find_spec(self.pip_name) is not None:
+            return True
+
+        try:
+            __import__(self.import_name)
+            return True
+        except ImportError:
+            pass
+
+        return False
+
+# List of packages to install (pip name, package name)
+required = [
+    Package('fonttools', ['woff', 'unicode'], 'fontTools'),
+    Package('brotli', [], 'brotli'),
+]
 
 # Get all packages that are not installed
-to_install = [ required[pip] for pip in required if not is_installed(pip) ]
+to_install = [ p.pip_package() for p in required if not p.is_installed() ]
 
 # Install all packages that are not installed
 if len(to_install) > 0:
