c0239752ef2e3ee1b83fcb0f0cc2f31ad685a5f8
hhvrc <github@heavenvr.tech>
2023-11-21 10:23:04 +0100

    Improve Json deserializer logging

--- src/serialization/JsonAPI.cpp
+++ src/serialization/JsonAPI.cpp
@@ -4,17 +4,19 @@
 
 const char* const TAG = "JsonAPI";
 
+#define ESP_LOGJSONE(err, root) ESP_LOGE(TAG, "Invalid JSON response (" err "): %s", cJSON_PrintUnformatted(root))
+
 using namespace OpenShock::Serialization;
 
 bool JsonAPI::ParseAccountLinkJsonResponse(int code, const cJSON* root, JsonAPI::AccountLinkResponse& out) {
   if (!cJSON_IsObject(root)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("not an object", root);
     return false;
   }
 
   const cJSON* data = cJSON_GetObjectItemCaseSensitive(root, "data");
   if (!cJSON_IsString(data)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("value at 'data' is not a string", root);
     return false;
   }
 
@@ -26,31 +28,31 @@ bool JsonAPI::ParseAccountLinkJsonResponse(int code, const cJSON* root, JsonAPI:
 }
 bool JsonAPI::ParseDeviceInfoJsonResponse(int code, const cJSON* root, JsonAPI::DeviceInfoResponse& out) {
   if (!cJSON_IsObject(root)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("not an object", root);
     return false;
   }
 
   const cJSON* data = cJSON_GetObjectItemCaseSensitive(root, "data");
   if (!cJSON_IsObject(data)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("value at 'data' is not an object", root);
     return false;
   }
 
   const cJSON* deviceId = cJSON_GetObjectItemCaseSensitive(data, "id");
   if (!cJSON_IsString(deviceId)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("value at 'data.id' is not a string", root);
     return false;
   }
 
   const cJSON* deviceName = cJSON_GetObjectItemCaseSensitive(data, "name");
   if (!cJSON_IsString(deviceName)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("value at 'data.name' is not a string", root);
     return false;
   }
 
   const cJSON* deviceShockers = cJSON_GetObjectItemCaseSensitive(data, "shockers");
   if (!cJSON_IsArray(deviceShockers)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("value at 'data.shockers' is not an array", root);
     return false;
   }
 
@@ -60,7 +62,7 @@ bool JsonAPI::ParseDeviceInfoJsonResponse(int code, const cJSON* root, JsonAPI::
   out.deviceName = deviceName->valuestring;
 
   if (out.deviceId.empty() || out.deviceName.empty()) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("value at 'data.id' or 'data.name' is empty", root);
     return false;
   }
 
@@ -68,37 +70,37 @@ bool JsonAPI::ParseDeviceInfoJsonResponse(int code, const cJSON* root, JsonAPI::
   cJSON_ArrayForEach(shocker, deviceShockers) {
     const cJSON* shockerId = cJSON_GetObjectItemCaseSensitive(shocker, "id");
     if (!cJSON_IsString(shockerId)) {
-      ESP_LOGE(TAG, "Invalid JSON response");
+      ESP_LOGJSONE("value at 'shocker.id' is not a string", shocker);
       return false;
     }
     const char* shockerIdStr = shockerId->valuestring;
 
     if (shockerIdStr == nullptr || shockerIdStr[0] == '\0') {
-      ESP_LOGE(TAG, "Invalid JSON response");
+      ESP_LOGJSONE("value at 'shocker.id' is empty", shocker);
       return false;
     }
 
     const cJSON* shockerRfId = cJSON_GetObjectItemCaseSensitive(shocker, "rfId");
     if (!cJSON_IsNumber(shockerRfId)) {
-      ESP_LOGE(TAG, "Invalid JSON response");
+      ESP_LOGJSONE("value at 'shocker.rfId' is not a number", shocker);
       return false;
     }
     int shockerRfIdInt = shockerRfId->valueint;
     if (shockerRfIdInt < 0 || shockerRfIdInt > UINT16_MAX) {
-      ESP_LOGE(TAG, "Invalid JSON response");
+      ESP_LOGJSONE("value at 'shocker.rfId' is not a valid uint16_t", shocker);
       return false;
     }
     std::uint16_t shockerRfIdU16 = (std::uint16_t)shockerRfIdInt;
 
     const cJSON* shockerModel = cJSON_GetObjectItemCaseSensitive(shocker, "model");
     if (!cJSON_IsString(shockerModel)) {
-      ESP_LOGE(TAG, "Invalid JSON response");
+      ESP_LOGJSONE("value at 'shocker.model' is not a string", shocker);
       return false;
     }
     const char* shockerModelStr = shockerModel->valuestring;
 
     if (shockerModelStr == nullptr || shockerModelStr[0] == '\0') {
-      ESP_LOGE(TAG, "Invalid JSON response");
+      ESP_LOGJSONE("value at 'shocker.model' is empty", shocker);
       return false;
     }
 
@@ -108,7 +110,7 @@ bool JsonAPI::ParseDeviceInfoJsonResponse(int code, const cJSON* root, JsonAPI::
     } else if (strcmp(shockerModelStr, "PetTrainer") == 0 || strcmp(shockerModelStr, "PT") == 0) {
       shockerModelType = OpenShock::ShockerModelType::PetTrainer;
     } else {
-      ESP_LOGE(TAG, "Invalid JSON response");
+      ESP_LOGJSONE("value at 'shocker.model' is not a valid shocker model", shocker);
       return false;
     }
 
@@ -119,13 +121,13 @@ bool JsonAPI::ParseDeviceInfoJsonResponse(int code, const cJSON* root, JsonAPI::
 }
 bool JsonAPI::ParseAssignLcgJsonResponse(int code, const cJSON* root, JsonAPI::AssignLcgResponse& out) {
   if (!cJSON_IsObject(root)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("not an object", root);
     return false;
   }
 
   const cJSON* data = cJSON_GetObjectItemCaseSensitive(root, "data");
   if (!cJSON_IsObject(data)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("value at 'data' is not an object", root);
     return false;
   }
 
@@ -133,7 +135,7 @@ bool JsonAPI::ParseAssignLcgJsonResponse(int code, const cJSON* root, JsonAPI::A
   const cJSON* country = cJSON_GetObjectItemCaseSensitive(data, "country");
 
   if (!cJSON_IsString(fqdn) || !cJSON_IsString(country)) {
-    ESP_LOGE(TAG, "Invalid JSON response");
+    ESP_LOGJSONE("value at 'data.fqdn' or 'data.country' is not a string", root);
     return false;
   }
 
