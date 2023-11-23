d20eaefa01ecdb3b5e456a167e22d62626433b4a
nullstalgia <nullstalgia@gmail.com>
2023-11-22 22:50:11 -0800

    Add OpenShock Core V1 definition, allow GPIO+RGB LEDs simultaneously, RGB Tweaks

--- src/VisualStateManager.cpp
+++ src/VisualStateManager.cpp
@@ -1,21 +1,17 @@
 #include "VisualStateManager.h"
 
 #ifdef OPENSHOCK_LED_GPIO
-#ifdef OPENSHOCK_LED_IMPLEMENTATION
-#error "Only one LED type can be defined at a time"
-#else
-#define OPENSHOCK_LED_IMPLEMENTATION
 #include "PinPatternManager.h"
 #include <Arduino.h>
+#ifndef OPENSHOCK_LED_IMPLEMENTATION
+#define OPENSHOCK_LED_IMPLEMENTATION
 #endif  // OPENSHOCK_LED_IMPLEMENTATION
 #endif  // OPENSHOCK_LED_GPIO
 
 #ifdef OPENSHOCK_LED_WS2812B
-#ifdef OPENSHOCK_LED_IMPLEMENTATION
-#error "Only one LED type can be defined at a time"
-#else
-#define OPENSHOCK_LED_IMPLEMENTATION
 #include "RGBPatternManager.h"
+#ifndef OPENSHOCK_LED_IMPLEMENTATION
+#define OPENSHOCK_LED_IMPLEMENTATION
 #endif  // OPENSHOCK_LED_IMPLEMENTATION
 #endif  // OPENSHOCK_LED_WS2812B
 
@@ -31,14 +27,18 @@
 
 const char* const TAG = "VisualStateManager";
 
-constexpr std::uint64_t kCriticalErrorFlag          = 1 << 0;
-constexpr std::uint64_t kEmergencyStoppedFlag       = 1 << 1;
-constexpr std::uint64_t kEmergencyStopClearedFlag   = 1 << 2;
-constexpr std::uint64_t kWebSocketConnectedFlag     = 1 << 3;
-constexpr std::uint64_t kWiFiConnectedWithoutWSFlag = 1 << 4;
-constexpr std::uint64_t kWiFiScanningFlag           = 1 << 5;
+constexpr std::uint64_t kCriticalErrorFlag        = 1 << 0;
+constexpr std::uint64_t kEmergencyStoppedFlag     = 1 << 1;
+constexpr std::uint64_t kEmergencyStopClearedFlag = 1 << 2;
+constexpr std::uint64_t kWebSocketConnectedFlag   = 1 << 3;
+constexpr std::uint64_t kWiFiConnectedFlag        = 1 << 4;
+constexpr std::uint64_t kWiFiScanningFlag         = 1 << 5;
 
 static std::uint64_t s_stateFlags = 0;
+
+// Bitmask of when the system is in a "all clear" state.
+
+constexpr std::uint64_t kStatusOKMask = kWebSocketConnectedFlag | kWiFiConnectedFlag;
 
 using namespace OpenShock;
 
@@ -55,8 +55,8 @@ constexpr PinPatternManager::State kEmergencyStoppedPattern[] = {
 };
 
 constexpr PinPatternManager::State kEmergencyStopClearedPattern[] = {
-  { true, 200},
-  {false, 200}
+  { true, 150},
+  {false, 150}
 };
 
 constexpr PinPatternManager::State kWiFiDisconnectedPattern[] = {
@@ -104,9 +104,21 @@ constexpr PinPatternManager::State kWebSocketConnectedPattern[] = {
   {false, 10'000}
 };
 
+constexpr PinPatternManager::State kSolidOnPattern[] = {
+  {true, 100'000}
+};
+
+constexpr PinPatternManager::State kSolidOffPattern[] = {
+  {false, 100'000}
+};
+
 PinPatternManager s_builtInLedManager(OPENSHOCK_LED_GPIO);
 
-void _updateVisualState() {
+void _updateVisualStateGPIO(nonstd::span<const PinPatternManager::State> override) {
+  s_builtInLedManager.SetPattern(override);
+}
+
+void _updateVisualStateGPIO() {
   if (s_stateFlags & kCriticalErrorFlag) {
     s_builtInLedManager.SetPattern(kCriticalErrorPattern);
     return;
@@ -127,7 +139,7 @@ void _updateVisualState() {
     return;
   }
 
-  if (s_stateFlags & kWiFiConnectedWithoutWSFlag) {
+  if (s_stateFlags & kWiFiConnectedFlag) {
     s_builtInLedManager.SetPattern(kWiFiConnectedWithoutWSPattern);
     return;
   }
@@ -155,58 +167,58 @@ constexpr RGBPatternManager::RGBState kEmergencyStoppedRGBPattern[] = {
 };
 
 constexpr RGBPatternManager::RGBState kEmergencyStopClearedRGBPattern[] = {
-  {0, 255, 0, 200},
-  {0,   0, 0, 200}
+  {0, 255, 0, 150},
+  {0,   0, 0, 150}
 };
 
 constexpr RGBPatternManager::RGBState kWiFiDisconnectedRGBPattern[] = {
-  {0, 0, 100, 100},
+  {0, 0, 255, 100},
   {0, 0,   0, 100},
-  {0, 0, 100, 100},
+  {0, 0, 255, 100},
   {0, 0,   0, 100},
-  {0, 0, 100, 100},
+  {0, 0, 255, 100},
   {0, 0,   0, 700}
 };
 
 constexpr RGBPatternManager::RGBState kWiFiConnectedWithoutWSRGBPattern[] = {
-  {0, 0, 100, 100},
-  {0, 0,   0, 100},
-  {0, 0, 100, 100},
-  {0, 0,   0, 700}
+  {255, 165, 0, 100},
+  {  0,   0, 0, 100},
+  {255, 165, 0, 100},
+  {  0,   0, 0, 700}
 };
 
 constexpr RGBPatternManager::RGBState kPingNoResponseRGBPattern[] = {
-  {0, 0, 100, 100},
-  {0, 0,   0, 100},
-  {0, 0, 100, 100},
-  {0, 0,   0, 100},
-  {0, 0, 100, 100},
-  {0, 0,   0, 100},
-  {0, 0, 100, 100},
-  {0, 0,   0, 700}
+  {0, 50, 255, 100},
+  {0,  0,   0, 100},
+  {0, 50, 255, 100},
+  {0,  0,   0, 100},
+  {0, 50, 255, 100},
+  {0,  0,   0, 100},
+  {0, 50, 255, 100},
+  {0,  0,   0, 700}
 };
 
 constexpr RGBPatternManager::RGBState kWebSocketCantConnectRGBPattern[] = {
-  {0, 0, 100, 100},
-  {0, 0,   0, 100},
-  {0, 0, 100, 100},
-  {0, 0,   0, 100},
-  {0, 0, 100, 100},
-  {0, 0,   0, 100},
-  {0, 0, 100, 100},
-  {0, 0,   0, 100},
-  {0, 0, 100, 100},
-  {0, 0,   0, 700}
+  {255, 0, 0, 100},
+  {  0, 0, 0, 100},
+  {255, 0, 0, 100},
+  {  0, 0, 0, 100},
+  {255, 0, 0, 100},
+  {  0, 0, 0, 100},
+  {255, 0, 0, 100},
+  {  0, 0, 0, 100},
+  {255, 0, 0, 100},
+  {  0, 0, 0, 700}
 };
 
 constexpr RGBPatternManager::RGBState kWebSocketConnectedRGBPattern[] = {
-  {0, 0, 100,    100},
-  {0, 0,   0, 10'000},
+  {0, 255, 0,    100},
+  {0,   0, 0, 10'000},
 };
 
 RGBPatternManager s_RGBLedManager(OPENSHOCK_LED_WS2812B);
 
-void _updateVisualState() {
+void _updateVisualStateRGB() {
   if (s_stateFlags & kCriticalErrorFlag) {
     s_RGBLedManager.SetPattern(kCriticalErrorRGBPattern);
     return;
@@ -227,7 +239,7 @@ void _updateVisualState() {
     return;
   }
 
-  if (s_stateFlags & kWiFiConnectedWithoutWSFlag) {
+  if (s_stateFlags & kWiFiConnectedFlag) {
     s_RGBLedManager.SetPattern(kWiFiConnectedWithoutWSRGBPattern);
     return;
   }
@@ -242,18 +254,32 @@ void _updateVisualState() {
 
 #endif  // OPENSHOCK_LED_WS2812B
 
-#ifndef OPENSHOCK_LED_IMPLEMENTATION
-
 void _updateVisualState() {
+#ifdef OPENSHOCK_LED_IMPLEMENTATION
+#if defined(OPENSHOCK_LED_GPIO) && defined(OPENSHOCK_LED_WS2812B)
+  if (s_stateFlags == kStatusOKMask) {
+    _updateVisualStateGPIO(kSolidOnPattern);
+  } else {
+    _updateVisualStateGPIO(kSolidOffPattern);
+  }
+  _updateVisualStateRGB();
+#elif defined(OPENSHOCK_LED_GPIO)
+  _updateVisualStateGPIO();
+#elif defined(OPENSHOCK_LED_WS2812B)
+  _updateVisualStateRGB();
+#else
+#error "No LED implementation selected but OPENSHOCK_LED_IMPLEMENTATION is defined"
+#endif
+#else
   ESP_LOGE(TAG, "_updateVisualState: (but no LED implementation is selected)");
+  vTaskDelay(10);
+#endif
 }
-
-#endif  // OPENSHOCK_LED_NONE
 
 void _handleWiFiConnected(arduino_event_t* event) {
   std::uint64_t oldState = s_stateFlags;
 
-  s_stateFlags |= kWiFiConnectedWithoutWSFlag;
+  s_stateFlags |= kWiFiConnectedFlag;
 
   if (oldState != s_stateFlags) {
     _updateVisualState();
@@ -262,7 +288,7 @@ void _handleWiFiConnected(arduino_event_t* event) {
 void _handleWiFiDisconnected(arduino_event_t* event) {
   std::uint64_t oldState = s_stateFlags;
 
-  s_stateFlags &= ~kWiFiConnectedWithoutWSFlag;
+  s_stateFlags &= ~kWiFiConnectedFlag;
 
   if (oldState != s_stateFlags) {
     _updateVisualState();
