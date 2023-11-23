5ef77d0a41ccc3a5f5a0eb980b4423ce981b9d62
nullstalgia <nullstalgia@gmail.com>
2023-11-22 22:49:43 -0800

    Basic WS2812B Implementation, set by -DOPENSHOCK_LED_WS2812B

--- src/VisualStateManager.cpp
+++ src/VisualStateManager.cpp
@@ -15,6 +15,7 @@
 #error "Only one LED type can be defined at a time"
 #else
 #define OPENSHOCK_LED_IMPLEMENTATION
+#include "RGBPatternManager.h"
 #endif  // OPENSHOCK_LED_IMPLEMENTATION
 #endif  // OPENSHOCK_LED_WS2812B
 
@@ -32,9 +33,10 @@ const char* const TAG = "VisualStateManager";
 
 constexpr std::uint64_t kCriticalErrorFlag          = 1 << 0;
 constexpr std::uint64_t kEmergencyStoppedFlag       = 1 << 1;
-constexpr std::uint64_t kWebSocketConnectedFlag     = 1 << 2;
-constexpr std::uint64_t kWiFiConnectedWithoutWSFlag = 1 << 3;
-constexpr std::uint64_t kWiFiScanningFlag           = 1 << 4;
+constexpr std::uint64_t kEmergencyStopClearedFlag   = 1 << 2;
+constexpr std::uint64_t kWebSocketConnectedFlag     = 1 << 3;
+constexpr std::uint64_t kWiFiConnectedWithoutWSFlag = 1 << 4;
+constexpr std::uint64_t kWiFiScanningFlag           = 1 << 5;
 
 static std::uint64_t s_stateFlags = 0;
 
@@ -52,7 +54,14 @@ constexpr PinPatternManager::State kEmergencyStoppedPattern[] = {
   {false, 500}
 };
 
+constexpr PinPatternManager::State kEmergencyStopClearedPattern[] = {
+  { true, 200},
+  {false, 200}
+};
+
 constexpr PinPatternManager::State kWiFiDisconnectedPattern[] = {
+  { true, 100},
+  {false, 100},
   { true, 100},
   {false, 100},
   { true, 100},
@@ -60,8 +69,6 @@ constexpr PinPatternManager::State kWiFiDisconnectedPattern[] = {
 };
 
 constexpr PinPatternManager::State kWiFiConnectedWithoutWSPattern[] = {
-  { true, 100},
-  {false, 100},
   { true, 100},
   {false, 100},
   { true, 100},
@@ -110,6 +117,11 @@ void _updateVisualState() {
     return;
   }
 
+  if (s_stateFlags & kEmergencyStopClearedFlag) {
+    s_builtInLedManager.SetPattern(kEmergencyStopClearedPattern);
+    return;
+  }
+
   if (s_stateFlags & kWebSocketConnectedFlag) {
     s_builtInLedManager.SetPattern(kWebSocketConnectedPattern);
     return;
@@ -132,8 +144,100 @@ void _updateVisualState() {
 
 #ifdef OPENSHOCK_LED_WS2812B
 
+constexpr RGBPatternManager::RGBState kCriticalErrorRGBPattern[] = {
+  {255, 0, 0, 100}, // Red ON for 0.1 seconds
+  {  0, 0, 0, 100}  // OFF for 0.1 seconds
+};
+
+constexpr RGBPatternManager::RGBState kEmergencyStoppedRGBPattern[] = {
+  {255, 0, 0, 500},
+  {  0, 0, 0, 500}
+};
+
+constexpr RGBPatternManager::RGBState kEmergencyStopClearedRGBPattern[] = {
+  {0, 255, 0, 200},
+  {0,   0, 0, 200}
+};
+
+constexpr RGBPatternManager::RGBState kWiFiDisconnectedRGBPattern[] = {
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 700}
+};
+
+constexpr RGBPatternManager::RGBState kWiFiConnectedWithoutWSRGBPattern[] = {
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 700}
+};
+
+constexpr RGBPatternManager::RGBState kPingNoResponseRGBPattern[] = {
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 700}
+};
+
+constexpr RGBPatternManager::RGBState kWebSocketCantConnectRGBPattern[] = {
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 100},
+  {0, 0, 100, 100},
+  {0, 0,   0, 700}
+};
+
+constexpr RGBPatternManager::RGBState kWebSocketConnectedRGBPattern[] = {
+  {0, 0, 100,    100},
+  {0, 0,   0, 10'000},
+};
+
+RGBPatternManager s_RGBLedManager(OPENSHOCK_LED_WS2812B);
+
 void _updateVisualState() {
-  ESP_LOGE(TAG, "_updateVisualState: (but WS2812B is not implemented yet)");
+  if (s_stateFlags & kCriticalErrorFlag) {
+    s_RGBLedManager.SetPattern(kCriticalErrorRGBPattern);
+    return;
+  }
+
+  if (s_stateFlags & kEmergencyStoppedFlag) {
+    s_RGBLedManager.SetPattern(kEmergencyStoppedRGBPattern);
+    return;
+  }
+
+  if (s_stateFlags & kEmergencyStopClearedFlag) {
+    s_RGBLedManager.SetPattern(kEmergencyStopClearedRGBPattern);
+    return;
+  }
+
+  if (s_stateFlags & kWebSocketConnectedFlag) {
+    s_RGBLedManager.SetPattern(kWebSocketConnectedRGBPattern);
+    return;
+  }
+
+  if (s_stateFlags & kWiFiConnectedWithoutWSFlag) {
+    s_RGBLedManager.SetPattern(kWiFiConnectedWithoutWSRGBPattern);
+    return;
+  }
+
+  if (s_stateFlags & kWiFiScanningFlag) {
+    s_RGBLedManager.SetPattern(kPingNoResponseRGBPattern);
+    return;
+  }
+
+  s_RGBLedManager.SetPattern(kWiFiDisconnectedRGBPattern);
 }
 
 #endif  // OPENSHOCK_LED_WS2812B
@@ -204,13 +308,30 @@ void VisualStateManager::SetScanningStarted() {
   }
 }
 
-void VisualStateManager::SetEmergencyStop(bool isStopped) {
+void VisualStateManager::SetEmergencyStop(OpenShock::EStopManager::EStopStatus status) {
   std::uint64_t oldState = s_stateFlags;
 
-  if (isStopped) {
-    s_stateFlags |= kEmergencyStoppedFlag;
-  } else {
-    s_stateFlags &= ~kEmergencyStoppedFlag;
+  switch (status) {
+    // When there is no EStop currently active.
+    case OpenShock::EStopManager::EStopStatus::ALL_CLEAR:
+      s_stateFlags &= ~kEmergencyStoppedFlag;
+      s_stateFlags &= ~kEmergencyStopClearedFlag;
+      break;
+    // EStop has been triggered!
+    case OpenShock::EStopManager::EStopStatus::ESTOPPED_AND_HELD:
+    // EStop still active, and user has released the button from the initial trigger.
+    case OpenShock::EStopManager::EStopStatus::ESTOPPED:
+      s_stateFlags |= kEmergencyStoppedFlag;
+      s_stateFlags &= ~kEmergencyStopClearedFlag;
+      break;
+    // User has held and cleared the EStop, now we're waiting for them to release the button.
+    case OpenShock::EStopManager::EStopStatus::ESTOPPED_CLEARED:
+      s_stateFlags &= ~kEmergencyStoppedFlag;
+      s_stateFlags |= kEmergencyStopClearedFlag;
+      break;
+    default:
+      ESP_LOGE(TAG, "Unhandled EStop status: %d", status);
+      break;
   }
 
   if (oldState != s_stateFlags) {
