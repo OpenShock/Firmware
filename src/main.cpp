55c6cd9be69a55f76c25812a645cd29b84da3493
hhvrc <github@heavenvr.tech>
2023-11-28 10:08:06 +0100

    Better startup sequence

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Logging.h"
 #include "OtaUpdateManager.h"
 #include "SerialInputHandler.h"
+#include "util/TaskUtils.h"
 #include "VisualStateManager.h"
 #include "wifi/WiFiManager.h"
 #include "wifi/WiFiScanManager.h"
@@ -18,16 +19,18 @@
 
 const char* const TAG = "OpenShock";
 
-void setup() {
-  Serial.begin(115'200);
+void setup_ota() {
+  OpenShock::OtaUpdateManager::Setup();
+}
+void main_ota(void* arg) {
+  while (true) {
+    OpenShock::OtaUpdateManager::Loop();
 
-  // Delegate startup to OtaManager if an ota update is ongoing
-  OpenShock::OtaUpdateManager::Init();
-  if (OpenShock::OtaUpdateManager::IsPerformingUpdate()) {
-    OpenShock::OtaUpdateManager::Setup();
-    return;
+    vTaskDelay(5);
   }
+}
 
+void setup_app() {
   if (!LittleFS.begin(true)) {
     ESP_PANIC(TAG, "Unable to mount LittleFS");
   }
@@ -53,15 +56,36 @@ void setup() {
     ESP_PANIC(TAG, "Unable to initialize GatewayConnectionManager");
   }
 }
+void main_app(void* arg) {
+  while (true) {
+    OpenShock::SerialInputHandler::Update();
+    OpenShock::CaptivePortal::Update();
+    OpenShock::GatewayConnectionManager::Update();
+    OpenShock::WiFiManager::Update();
+
+    vTaskDelay(5);
+  }
+}
+
+void setup() {
+  Serial.begin(115'200);
+
+  OpenShock::OtaUpdateManager::Init();
+  if (OpenShock::OtaUpdateManager::IsPerformingUpdate()) {
+    setup_ota();
+  } else {
+    setup_app();
+  }
+}
 
 void loop() {
+  // Start the main task
   if (OpenShock::OtaUpdateManager::IsPerformingUpdate()) {
-    OpenShock::OtaUpdateManager::Loop();
-    return;
+    OpenShock::TaskUtils::TaskCreateExpensive(main_ota, "main_ota", 8192, nullptr, 1, nullptr);
+  } else {
+    OpenShock::TaskUtils::TaskCreateExpensive(main_app, "main_app", 8192, nullptr, 1, nullptr);
   }
 
-  OpenShock::SerialInputHandler::Update();
-  OpenShock::CaptivePortal::Update();
-  OpenShock::GatewayConnectionManager::Update();
-  OpenShock::WiFiManager::Update();
+  // Kill the loop task
+  vTaskDelete(nullptr);
 }
