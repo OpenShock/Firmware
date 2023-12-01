c77393c61e18833b45f895cf2d83a966263cf554
hhvrc <github@heavenvr.tech>
2023-12-01 09:25:56 +0100

    Fix base64 issues

--- include/util/Base64Utils.h
+++ include/util/Base64Utils.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#include <mbedtls/base64.h>
-
 #include <cstdint>
 #include <cstring>
 #include <string>
+#include <vector>
 
 // IMPORTANT: WE DONT USE ARRAYS OR TEMPLATES HERE
 
@@ -13,14 +12,14 @@ namespace OpenShock::Base64Utils {
   /// @param size The size of the data to encode.
   /// @return The size of the buffer required to hold the base64 encoded data.
   constexpr std::size_t CalculateEncodedSize(std::size_t size) noexcept {
-    return (size + 2) / 3 * 4;
+    return (4 * (size / 3)) + 4; // TODO: This is wrong, but what mbedtls requires???
   }
 
   /// @brief Calculates the size of the buffer required to hold the decoded data.
   /// @param size The size of the data to decode.
   /// @return The size of the buffer required to hold the decoded data.
   constexpr std::size_t CalculateDecodedSize(std::size_t size) noexcept {
-    return (size + 3) / 4 * 3;
+    return 3 * size / 4;
   }
 
   /// @brief Encodes a byte array to base64.
@@ -29,24 +28,14 @@ namespace OpenShock::Base64Utils {
   /// @param output
   /// @param outputLen
   /// @return The amount of bytes written to the output buffer.
-  inline std::size_t Encode(const std::uint8_t* data, std::size_t dataLen, char* output, std::size_t outputLen) noexcept {
-    std::size_t written = 0;
-    if (mbedtls_base64_encode(reinterpret_cast<std::uint8_t*>(output), outputLen, &written, data, dataLen) != 0) {
-      return 0;
-    }
-    return written;
-  }
+  std::size_t Encode(const std::uint8_t* data, std::size_t dataLen, char* output, std::size_t outputLen) noexcept;
 
   /// @brief Encodes a byte array to base64.
   /// @param data The data to encode.
   /// @param dataLen The size of the data to encode.
   /// @param output The output string to write to.
   /// @return The amount of bytes written to the output buffer.
-  inline bool Encode(const std::uint8_t* data, std::size_t dataLen, std::string& output) noexcept {
-    std::size_t outputLen = CalculateEncodedSize(dataLen);
-    output.resize(outputLen);
-    return Encode(data, dataLen, const_cast<char*>(output.data()), output.size()) != 0;
-  }
+  bool Encode(const std::uint8_t* data, std::size_t dataLen, std::string& output) noexcept;
 
   /// @brief Decodes a base64 string.
   /// @param data The data to decode.
@@ -54,22 +43,12 @@ namespace OpenShock::Base64Utils {
   /// @param output The output buffer to write to.
   /// @param outputLen The size of the output buffer.
   /// @return The amount of bytes written to the output buffer.
-  inline std::size_t Decode(const char* data, std::size_t dataLen, std::uint8_t* output, std::size_t outputLen) noexcept {
-    std::size_t written = 0;
-    if (mbedtls_base64_decode(output, outputLen, &written, reinterpret_cast<const std::uint8_t*>(data), dataLen) != 0) {
-      return 0;
-    }
-    return written;
-  }
+  std::size_t Decode(const char* data, std::size_t dataLen, std::uint8_t* output, std::size_t outputLen) noexcept;
 
   /// @brief Decodes a base64 string.
   /// @param data The data to decode.
   /// @param dataLen The size of the data to decode.
   /// @param output The output buffer to write to.
   /// @return The amount of bytes written to the output buffer.
-  inline bool Decode(const char* data, std::size_t dataLen, std::vector<std::uint8_t>& output) noexcept {
-    std::size_t outputLen = CalculateDecodedSize(dataLen);
-    output.resize(outputLen);
-    return Decode(data, dataLen, output.data(), outputLen) != 0;
-  }
+  bool Decode(const char* data, std::size_t dataLen, std::vector<std::uint8_t>& output) noexcept;
 }  // namespace OpenShock::Base64Utils
