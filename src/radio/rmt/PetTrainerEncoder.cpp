2e36c26633bac4705e285c99b41b5a3f8e6775d1
hhvrc <github@heavenvr.tech>
2023-12-04 15:07:44 +0100

    Simplify (ish) Petrainer encoder

--- src/radio/rmt/PetTrainerEncoder.cpp
+++ src/radio/rmt/PetTrainerEncoder.cpp
@@ -10,13 +10,32 @@ const rmt_data_t kRmtPostamble = {200, 1, 7000, 0};
 using namespace OpenShock;
 
 std::vector<rmt_data_t> Rmt::PetTrainerEncoder::GetSequence(std::uint16_t shockerId, ShockerCommandType type, std::uint8_t intensity) {
-  // Intensity must be between 0 and ?? (guessing 99)
-  intensity = std::min(intensity, (std::uint8_t)99);
+  // Intensity must be between 0 and 100
+  intensity = std::min(intensity, (std::uint8_t)100);
 
-  std::uint8_t methodBit      = (0x80 | (1 << ((std::uint8_t)type - 1))) & 0xFF;
-  std::uint8_t methodChecksum = 0xFF ^ ((1 << (8 - (std::uint8_t)type)) | 1);
+  std::uint8_t nShift = 0;
+  switch (type) {
+  case ShockerCommandType::Shock:
+    nShift = 0;
+    break;
+  case ShockerCommandType::Vibrate:
+    nShift = 1;
+    break;
+  case ShockerCommandType::Sound:
+    nShift = 2;
+    break;
+  default:
+    return {}; // Invalid type
+  }
 
-  std::uint64_t data = (std::uint64_t(methodBit) << 32) | (std::uint64_t(shockerId) << 16) | (std::uint64_t(intensity) << 8) | (std::uint64_t(methodChecksum) << 0);
+  // Type is 0x80 | (0x01 << nShift)
+  std::uint8_t typeVal = (0x80 | (0x01 << nShift)) & 0xFF;
+
+  // TypeSum is NOT(0x01 | (0x80 >> nShift))
+  std::uint8_t typeSum = (~(0x01 | (0x80 >> nShift))) & 0xFF;
+
+  // Payload layout: [methodBit:8][shockerId:16][intensity:8][methodChecksum:8]
+  std::uint64_t data = (std::uint64_t(typeVal) << 32) | (std::uint64_t(shockerId) << 16) | (std::uint64_t(intensity) << 8) | std::uint64_t(typeSum);
 
   std::vector<rmt_data_t> pulses;
   pulses.reserve(42);
