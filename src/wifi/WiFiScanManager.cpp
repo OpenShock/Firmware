260ab55dfcbf45e6a02abb2266ef02dc00142c02
HentaiHeavenVR <github@heavenvr.tech>
2023-12-11 12:29:34 +0100

    Remove unneccesary locking

--- src/wifi/WiFiScanManager.cpp
+++ src/wifi/WiFiScanManager.cpp
@@ -21,6 +21,7 @@ enum WiFiScanTaskNotificationFlags {
 
 using namespace OpenShock;
 
+static bool s_initialized = false;
 static TaskHandle_t s_scanTaskHandle     = nullptr;
 static SemaphoreHandle_t s_scanTaskMutex = xSemaphoreCreateMutex();
 static std::uint8_t s_currentChannel     = 0;
@@ -185,16 +186,15 @@ void _evSTAStopped(arduino_event_id_t event, arduino_event_info_t info) {
 }
 
 bool WiFiScanManager::Init() {
-  // Initialize the scan semaphore
-  if (xSemaphoreTake(s_scanTaskMutex, 0) != pdTRUE) {
-    ESP_LOGE(TAG, "Initialize function called more than once");
-    return false;
+  if (s_initialized) {
+    ESP_LOGW(TAG, "WiFiScanManager is already initialized");
+    return true;
   }
 
   WiFi.onEvent(_evScanCompleted, ARDUINO_EVENT_WIFI_SCAN_DONE);
   WiFi.onEvent(_evSTAStopped, ARDUINO_EVENT_WIFI_STA_STOP);
 
-  xSemaphoreGive(s_scanTaskMutex);
+  s_initialized = true;
 
   return true;
 }
