304c61447fb8a0f70daa70a17effcf6d034fa8c1
nullstalgia <nullstalgia@gmail.com>
2023-12-10 19:34:23 -0800

    Have ALL chips use the same 4MB OTA partition table

--- chips/ESP32-S2/4MB/merge-image.py
+++ chips/ESP32-S2/4MB/merge-image.py
@@ -4,12 +4,12 @@ import esptool
 
 # fmt: off
 esptool.main([
-    '--chip', 'esp32',
+    '--chip', 'esp32s2',
     'merge_bin', '-o', 'merged.bin',
     '--flash_size', '4MB',
     '0x1000', './bootloader.bin',
     '0x8000', './partitions.bin',
     '0x10000', './firmware.bin',
-    '0x310000', './filesystem.bin'
+    '0x353000', './filesystem.bin'
 ])
 # fmt: on
--- scripts/use_openshock_params.py
+++ scripts/use_openshock_params.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from utils.boardconf import BoardConf, from_pio_env, validate, print_header, print_footer
+from utils.boardconf import BoardConf, from_pio_env, validate, print_header, print_footer, fallback_flash_size
 import csv
 
 #
@@ -20,58 +20,33 @@ env = env  # type: ignore
 # Parse the board/chip specific configuration from the current pio `env` variable.
 boardconf: BoardConf = from_pio_env(env)
 
-config_size = 0x3000
-
-def update_partitions_file(partitions_file: Path):
-    new_partitions_file = partitions_file.parent / 'partitions_modified.csv'
-    partitions_file_path = str(partitions_file)
-    updated_partitions = []
-    spiffs_found = False
-
-    with open(partitions_file_path, 'r') as file:
-        reader = csv.reader(file, skipinitialspace=True)
-        for row in reader:
-            if row and row[0].lower() in ['spiffs', 'littlefs']:
-                spiffs_found = True
-
-                fs_name, fs_part_type, fs_part_subtype, fs_offset, fs_size, fs_flags = row
-                # Making config partition to be added before existing FS partition
-                config_partition = ['config', 'data', 'spiffs', fs_offset, hex(config_size)]
-                # Forcing FS label to be 'littlefs'
-                updated_fs_partition = ['littlefs', fs_part_type, fs_part_subtype, hex(int(fs_offset, 16) + config_size), hex(int(fs_size, 16) - config_size)]
-
-                # Config partition should be specified before FS partition
-                # Because PlatformIO uploads to the last partition with 'spiffs' subtype
-                updated_partitions.append(config_partition)
-                updated_partitions.append(updated_fs_partition)
-            else:
-                updated_partitions.append(row)
-
-    if spiffs_found:
-        with open(str(new_partitions_file), 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(updated_partitions)
-        return str(new_partitions_file)
-    else:
-        print('WARNING: SPIFFS/LittleFS PARTITION NOT FOUND.')
-        print('Not overriding default value.')
-        return str(partitions_file)
+# In case we need to blacklist certain flash sizes from OTA support.
+blacklisted_OTA_sizes = [
+    # '4MB'
+    ]
 
 def use_openshock_params():
     if not validate(boardconf):
         return
 
     # Handle partitions file not existing
-    if not boardconf.does_partitions_file_exists():
-        print('WARNING: PARTITIONS FILE DOES NOT EXIST.')
-        print('Not overriding default value.')
-        return
+    if not boardconf.do_partitions_files_exists(boardconf.get_flash_size()):
+        if boardconf.do_partitions_files_exists(fallback_flash_size):
+            print(f'WARNING: PARTITIONS FILES DON\'T EXIST FOR THIS FLASH SIZE, FALLING BACK TO {fallback_flash_size}.')
+            boardconf.override_flash_size(fallback_flash_size)
 
     # Set partition file to chip dir.
     # https://docs.platformio.org/en/latest/scripting/examples/override_board_configuration.html
     board_config = env.BoardConfig()
-    new_partitions_file = update_partitions_file(boardconf.get_partitions_file())
-    board_config.update('build.partitions', new_partitions_file)
+
+    partitions, partitions_ota = boardconf.get_partitions_files(boardconf.get_flash_size())
+
+    if boardconf.get_flash_size() in blacklisted_OTA_sizes:
+        print('WARNING: OTA NOT SUPPORTED FOR THIS FLASH SIZE.')
+        print('Using Non-OTA partition layout.')
+        board_config.update('build.partitions', str(partitions))
+    else:   
+        board_config.update('build.partitions', str(partitions_ota))
 
 
 print_header()
