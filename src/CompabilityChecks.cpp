0489190d03f402024463523227f81501df5a3571
HentaiHeavenVR <github@heavenvr.tech>
2024-01-03 13:45:36 +0100

    Add compatibility check for estop pin

--- src/CompabilityChecks.cpp
+++ src/CompabilityChecks.cpp
@@ -4,6 +4,10 @@
 constexpr bool kIsValidRfTxPin = OpenShock::IsValidOutputPin(OPENSHOCK_RF_TX_GPIO) || OPENSHOCK_RF_TX_GPIO == UINT8_MAX;
 static_assert(kIsValidRfTxPin , "OPENSHOCK_RF_TX_GPIO is not a valid output GPIO, and is not declared as bypassed by board specific definitions, refusing to compile");
 
+#ifdef OPENSHOCK_ESTOP_PIN
+static_assert(OpenShock::IsValidInputPin(OPENSHOCK_ESTOP_PIN), "OPENSHOCK_ESTOP_PIN is not a valid input GPIO, and is not declared as bypassed by board specific definitions, refusing to compile");
+#endif
+
 #ifdef OPENSHOCK_LED_GPIO
 static_assert(OpenShock::IsValidOutputPin(OPENSHOCK_LED_GPIO), "OPENSHOCK_LED_GPIO is not a valid output GPIO, and is not declared as bypassed by board specific definitions, refusing to compile");
 #endif
