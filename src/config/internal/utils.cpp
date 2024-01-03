d0f1e8f9758e449b576e6ef3ab1330e2d5c20d01
HentaiHeavenVR <github@heavenvr.tech>
2024-01-03 13:37:09 +0100

    Better error checking on config parsing

--- src/config/internal/utils.cpp
+++ src/config/internal/utils.cpp
@@ -6,6 +6,38 @@ const char* const TAG = "Config::Internal::Utils";
 
 using namespace OpenShock;
 
+template<typename T>
+bool _utilFromJsonInt(T& val, const cJSON* json, const char* name, T defaultVal, int minVal, int maxVal) {
+  static_assert(std::is_integral<T>::value, "T must be an integral type");
+
+  const cJSON* jsonVal = cJSON_GetObjectItemCaseSensitive(json, name);
+  if (jsonVal == nullptr) {
+    val = defaultVal;
+    return true;
+  }
+
+  if (cJSON_IsNumber(jsonVal) == 0) {
+    ESP_LOGE(TAG, "value at '%s' is not a number", name);
+    return false;
+  }
+
+  int intVal = jsonVal->valueint;
+
+  if (intVal < minVal) {
+    ESP_LOGE(TAG, "value at '%s' is less than %d", name, minVal);
+    return false;
+  }
+
+  if (intVal > maxVal) {
+    ESP_LOGE(TAG, "value at '%s' is greater than %d", name, maxVal);
+    return false;
+  }
+
+  val = static_cast<T>(intVal);
+
+  return true;
+}
+
 void Config::Internal::Utils::FromFbsStr(std::string& str, const flatbuffers::String* fbsStr, const char* defaultStr) {
   if (fbsStr != nullptr) {
     str = fbsStr->c_str();
@@ -14,82 +46,44 @@ void Config::Internal::Utils::FromFbsStr(std::string& str, const flatbuffers::St
   }
 }
 
-void Config::Internal::Utils::FromJsonBool(bool& val, const cJSON* json, const char* name, bool defaultVal) {
-  const cJSON* jsonVal = cJSON_GetObjectItemCaseSensitive(json, name);
-  if (jsonVal != nullptr) {
-    if (cJSON_IsBool(jsonVal) == 0) {
-      ESP_LOGE(TAG, "value at '%s' is not a bool", name);
-      return;
-    }
-    val = cJSON_IsTrue(jsonVal);
-  } else {
-    val = defaultVal;
-  }
-}
-
-void Config::Internal::Utils::FromJsonU8(std::uint8_t& val, const cJSON* json, const char* name, std::uint8_t defaultVal) {
+bool Config::Internal::Utils::FromJsonBool(bool& val, const cJSON* json, const char* name, bool defaultVal) {
   const cJSON* jsonVal = cJSON_GetObjectItemCaseSensitive(json, name);
   if (jsonVal == nullptr) {
     val = defaultVal;
-    return;
+    return true;
   }
 
-  if (cJSON_IsNumber(jsonVal) == 0) {
-    ESP_LOGE(TAG, "value at '%s' is not a number", name);
-    return;
+  if (cJSON_IsBool(jsonVal) == 0) {
+    ESP_LOGE(TAG, "value at '%s' is not a bool", name);
+    return false;
   }
 
-  int intVal = jsonVal->valueint;
+  val = cJSON_IsTrue(jsonVal);
 
-  if (intVal < 0) {
-    ESP_LOGE(TAG, "value at '%s' is negative", name);
-    return;
-  }
-
-  if (intVal > UINT8_MAX) {
-    ESP_LOGE(TAG, "value at '%s' is greater than UINT8_MAX", name);
-    return;
-  }
-
-  val = static_cast<std::uint8_t>(intVal);
+  return true;
 }
 
-void Config::Internal::Utils::FromJsonU16(std::uint16_t& val, const cJSON* json, const char* name, std::uint16_t defaultVal) {
+bool Config::Internal::Utils::FromJsonU8(std::uint8_t& val, const cJSON* json, const char* name, std::uint8_t defaultVal) {
+  return _utilFromJsonInt(val, json, name, defaultVal, 0, UINT8_MAX);
+}
+
+bool Config::Internal::Utils::FromJsonU16(std::uint16_t& val, const cJSON* json, const char* name, std::uint16_t defaultVal) {
+  return _utilFromJsonInt(val, json, name, defaultVal, 0, UINT16_MAX);
+}
+
+bool Config::Internal::Utils::FromJsonStr(std::string& str, const cJSON* json, const char* name, const char* defaultStr) {
   const cJSON* jsonVal = cJSON_GetObjectItemCaseSensitive(json, name);
   if (jsonVal == nullptr) {
-    val = defaultVal;
-    return;
-  }
-
-  if (cJSON_IsNumber(jsonVal) == 0) {
-    ESP_LOGE(TAG, "value at '%s' is not a number", name);
-    return;
-  }
-
-  int intVal = jsonVal->valueint;
-
-  if (intVal < 0) {
-    ESP_LOGE(TAG, "value at '%s' is negative", name);
-    return;
-  }
-
-  if (intVal > UINT16_MAX) {
-    ESP_LOGE(TAG, "value at '%s' is greater than UINT16_MAX", name);
-    return;
-  }
-
-  val = static_cast<std::uint16_t>(intVal);
-}
-
-void Config::Internal::Utils::FromJsonStr(std::string& str, const cJSON* json, const char* name, const char* defaultStr) {
-  const cJSON* jsonVal = cJSON_GetObjectItemCaseSensitive(json, name);
-  if (jsonVal != nullptr) {
-    if (cJSON_IsString(jsonVal) == 0) {
-      ESP_LOGE(TAG, "value at '%s' is not a string", name);
-      return;
-    }
-    str = jsonVal->valuestring;
-  } else {
     str = defaultStr;
+    return true;
   }
+
+  if (cJSON_IsString(jsonVal) == 0) {
+    ESP_LOGE(TAG, "value at '%s' is not a string", name);
+    return false;
+  }
+
+  str = jsonVal->valuestring;
+
+  return true;
 }
