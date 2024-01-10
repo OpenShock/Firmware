f93ffc8c781fdb0d0d66ff70a243df911df0db42
HentaiHeavenVR <github@heavenvr.tech>
2024-01-10 19:08:39 +0100

    More simplification

--- .github/scripts/get-vars.js
+++ .github/scripts/get-vars.js
@@ -4,32 +4,60 @@ const semver = require('semver');
 const core = require('@actions/core');
 const child_process = require('child_process');
 
-function getGitBranch() {
-  // Get branch name
-  const gitRef = process.env.GITHUB_REF;
-  if (!gitRef) {
-    core.setFailed('Environment variable "GITHUB_REF" not found');
+// Get branch name
+const gitRef = process.env.GITHUB_REF;
+if (gitRef === undefined) {
+  core.setFailed('Environment variable "GITHUB_REF" not found');
+  process.exit();
+}
+
+const isGitTag = gitRef.startsWith('refs/tags/');
+const isGitBranch = gitRef.startsWith('refs/heads/');
+const isGitPullRequest = gitRef.startsWith('refs/pull/') && gitRef.endsWith('/merge');
+const gitCommitHash = child_process.execSync('git rev-parse HEAD').toString().trim();
+const gitShortCommitHash = child_process.execSync('git rev-parse --short HEAD').toString().trim();
+
+if (!isGitTag && !isGitBranch && !isGitPullRequest) {
+  core.setFailed(`Git ref "${gitRef}" is not a valid branch, tag or pull request`);
+  process.exit();
+}
+
+const gitHeadRefName = isGitPullRequest ? process.env.GITHUB_HEAD_REF : gitRef.split('/')[2];
+if (gitHeadRefName === undefined) {
+  core.setFailed('Failed to get git head ref name');
+  process.exit();
+}
+
+const latestGitTag = isGitTag ? gitRef.split('/')[2] : child_process.execSync('git for-each-ref --sort=-creatordate --count=1 --format "%(refname:short)" refs/tags').toString().trim();
+if (latestGitTag === undefined) {
+  core.setFailed('Failed to get latest git tag');
+  process.exit();
+}
+
+function getCurrentVersion() {
+  // Parse and validate latest git tag
+  const parsed = semver.parse(latestGitTag === '' ? '0.0.0' : latestGitTag);
+  if (parsed === null || parsed.loose) {
+    core.setFailed(`Latest git tag "${latestGitTag}" is not a valid semver version`);
     process.exit();
   }
 
-  if (!gitRef.startsWith('refs/heads/')) {
-    return null;
+  // Build version string
+  let base = `${parsed.major}.${parsed.minor}.${parsed.patch}`;
+  if (!isGitTag) {
+    base += `-${gitHeadRefName}+${gitShortCommitHash}`;
   }
 
-  return gitRef.substring(11);
+  return base;
 }
-function getGitCommit() {
-  try {
-    return child_process.execSync('git rev-parse --short HEAD').toString().trim();
-  } catch (error) {
-    return 'unknown';
-  }
-}
-function getVersionChangeLog(lines, version, isMaster) {
+
+const version = getCurrentVersion();
+
+function getVersionChangeLog(lines) {
   const emptyChangelog = lines.length === 0;
 
   // Enforce that the changelog is not empty if we are on the master branch
-  if (isMaster && emptyChangelog) {
+  if (isGitTag && emptyChangelog) {
     core.setFailed('File "CHANGELOG.md" is empty, this must be populated in the master branch');
     process.exit();
   }
@@ -46,13 +74,13 @@ function getVersionChangeLog(lines, version, isMaster) {
 
   // Get the start of the entry
   const changeLogBegin = lines.findIndex((line) => line.startsWith(`# Version ${version}`));
-  if (isMaster && changeLogBegin === -1) {
+  if (isGitTag && changeLogBegin === -1) {
     core.setFailed(`File "CHANGELOG.md" does not contain a changelog entry for version "${version}", this must be added in the master branch`);
     process.exit();
   }
 
   // Enforce that the changelog entry is at the top of the file if we are on the master branch
-  if (isMaster && changeLogBegin !== 0) {
+  if (isGitTag && changeLogBegin !== 0) {
     core.setFailed(`Changelog entry for version "${version}" is not at the top of the file, you tag is either out of date or you have not updated the changelog`);
     process.exit();
   }
@@ -68,17 +96,13 @@ function getVersionChangeLog(lines, version, isMaster) {
   const emptyChangelogEntry = lines.slice(changeLogBegin + 1, changeLogEnd).filter((line) => line.trim() !== '').length === 0;
 
   // Enforce that the changelog entry is not empty if we are on the master branch
-  if (isMaster && emptyChangelogEntry) {
+  if (isGitTag && emptyChangelogEntry) {
     core.setFailed(`Changelog entry for version "${version}" is empty, this must be populated in the master branch`);
     process.exit();
   }
 
   return lines.slice(changeLogBegin, changeLogEnd).join('\n');
 }
-
-const gitBranch = getGitBranch();
-const isMaster = gitBranch === 'master';
-const isDevelop = gitBranch === 'develop';
 
 // Make sure we have all the files we need
 for (const file of ['RELEASE.md', 'CHANGELOG.md', 'platformio.ini']) {
@@ -88,40 +112,21 @@ for (const file of ['RELEASE.md', 'CHANGELOG.md', 'platformio.ini']) {
   }
 }
 
-// Read files "VERSION" and "CHANGELOG.md"
-let release = fs.readFileSync('RELEASE.md', 'utf8');
+// Read files
+let releaseNotes = fs.readFileSync('RELEASE.md', 'utf8');
 let fullChangelog = fs.readFileSync('CHANGELOG.md', 'utf8').trim();
 let platformioIniStr = fs.readFileSync('platformio.ini', 'utf8').trim();
-
-// Pull history from git
-child_process.execSync('git fetch --unshallow');
-
-// Fetch latest git tag
-let version = child_process.execSync('git describe --tags --abbrev=0').toString().trim();
-
-// Parse and validate version
-let parsedVersion = semver.parse(version);
-if (parsedVersion === null || parsedVersion.loose) {
-  core.setFailed(`Latest git tag "${version}" is not a valid semver version`);
-  process.exit();
-}
-
-// Rebuild version string
-version = `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch}`;
-if (!isMaster) {
-  version += `-${gitBranch.replace(/[^a-z0-9]/gi, '.')}+${getGitCommit()}`;
-}
 
 const changelogLines = fullChangelog.split('\n');
 
 // Get the changelog for the current version
-const versionChangeLog = getVersionChangeLog(changelogLines, version, isMaster);
+const versionChangeLog = getVersionChangeLog(changelogLines);
 
 // Finish building the release string
-release = versionChangeLog ? versionChangeLog + '\n\n' + release : release;
+releaseNotes = versionChangeLog ? versionChangeLog + '\n\n' + releaseNotes : releaseNotes;
 
 // Get all versions from the changelog
-const changelogVersions = changelogLines.filter((line) => line.startsWith('# Version ')).map((line) => line.substring(10).split(' ')[0].trim());
+const releases = changelogLines.filter((line) => line.startsWith('# Version ')).map((line) => line.substring(10).split(' ')[0].trim());
 
 // Parse platformio.ini and extract the different boards
 let platformioIni = ini.parse(platformioIniStr);
@@ -134,13 +139,17 @@ let boards = Object.keys(platformioIni)
     return arr;
   }, []);
 
+console.log('Version:  ' + version);
+console.log('Boards:   ' + boards.join(', '));
+console.log('Releases: ' + releases.join(', '));
+
 // Set outputs
 core.setOutput('version', version);
-core.setOutput('release', release);
 core.setOutput('changelog', versionChangeLog);
+core.setOutput('release-notes', releaseNotes);
 core.setOutput('full-changelog', fullChangelog);
 core.setOutput('board-list', boards.join('\n'));
 core.setOutput('board-array', JSON.stringify(boards));
 core.setOutput('board-matrix', JSON.stringify({ board: boards }));
-core.setOutput('version-list', changelogVersions.join('\n'));
-core.setOutput('version-array', JSON.stringify(changelogVersions));
+core.setOutput('release-list', releases.join('\n'));
+core.setOutput('release-array', JSON.stringify(releases));
