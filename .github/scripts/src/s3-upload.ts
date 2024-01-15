e77e8de001aa46adbb94b6389db156a0ab721ce9
HentaiHeavenVR <github@heavenvr.tech>
2024-01-15 15:56:21 +0100

    Implement stream support

--- .github/scripts/src/s3-upload.ts
+++ .github/scripts/src/s3-upload.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as stream from 'node:stream';
 import mime from 'mime';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { setFailed } from '@actions/core';
@@ -24,6 +25,14 @@ const S3 = new S3Client({
   },
 });
 
+async function* walk(dir: string): AsyncGenerator<string> {
+  for await (const d of await fs.promises.opendir(dir)) {
+    const fileName = path.join(dir, d.name);
+    if (d.isDirectory()) yield* walk(fileName);
+    else if (d.isFile()) yield fileName;
+  }
+}
+
 function getFileHashMD5(fileName: string): string {
   const md5sum = child_process.execSync(`md5sum ${fileName}`).toString().trim();
 
@@ -35,7 +44,21 @@ function getFileHashMD5(fileName: string): string {
   return md5sum.split(' ')[0];
 }
 
-async function uploadFile(fileName: string, contentTypeMime: string | null, remoteFileKey: string) {
+async function uploadStream(Body: stream.Readable, Key: string, ContentLength: number | undefined = undefined, contentTypeMime: string | null = null, ContentMD5: string | undefined = undefined) {
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key,
+    Body,
+    ContentType: contentTypeMime ?? 'application/octet-stream',
+    ContentLength,
+    ContentMD5,
+  });
+
+  const response = await S3.send(command);
+
+  console.log(response);
+}
+async function uploadFile(fileName: string, remoteFileKey: string, contentTypeMime: string | null) {
   // Check if file exists
   if (!fs.existsSync(fileName)) {
     setFailed(`File ${fileName} does not exist`);
@@ -48,24 +71,36 @@ async function uploadFile(fileName: string, contentTypeMime: string | null, remo
     process.exit();
   }
 
-  const command = new PutObjectCommand({
-    Bucket: bucketName,
-    Key: remoteFileKey,
-    Body: fs.createReadStream(fileName),
-    ContentType: contentTypeMime ?? mime.getType(fileName) ?? 'application/octet-stream',
-    ContentLength: fs.statSync(fileName).size,
-    ContentMD5: getFileHashMD5(fileName),
-  });
+  await uploadStream(fs.createReadStream(fileName), remoteFileKey, fs.statSync(fileName).size, contentTypeMime ?? mime.getType(fileName), getFileHashMD5(fileName));
+}
+async function uploadDir(dirName: string, remoteDirKey: string) {
+  // Check if directory exists
+  if (!fs.existsSync(dirName)) {
+    setFailed(`Directory ${dirName} does not exist`);
+    process.exit();
+  }
 
-  const response = await S3.send(command);
+  // Check if directory is readable
+  if (!fs.statSync(dirName).isDirectory()) {
+    setFailed(`Directory ${dirName} is not readable`);
+    process.exit();
+  }
 
-  console.log(response);
+  for await (const fileName of walk(dirName)) {
+    const remoteFileKey = `${remoteDirKey}/${fileName.replace(dirName, '')}`;
+    await uploadFile(fileName, remoteFileKey, null);
+  }
+}
+async function uploadStdin(remoteFileKey: string, contentTypeMime: string | null) {
+  await uploadStream(process.stdin, remoteFileKey, undefined, contentTypeMime);
 }
 
 // Usage:
-//    node s3-upload.js <dir-name> <remote-dir-key>
-//    node s3-upload.js <file-name> <remote-file-key>
-//    node s3-upload.js <file-name> <content-type> <remote-file-key>
+//    node s3-upload.js dir   <dir-name> <remote-dir-key>
+//    node s3-upload.js file  <file-name> <remote-file-key>
+//    node s3-upload.js file  <file-name> <remote-file-key> <content-type>
+//    node s3-upload.js stdin <remote-file-key>
+//    node s3-upload.js stdin <remote-file-key> <content-type>
 
 // Process arguments
 const args = process.argv.slice(2);
@@ -74,34 +109,29 @@ if (args.length < 2 || args.length > 3) {
   process.exit();
 }
 
-let fileOrDirName: string;
-let contentTypeMime: string | null = null;
-let remoteFileOrDirKey: string;
-
-if (args.length === 2) {
-  [fileOrDirName, remoteFileOrDirKey] = args;
-} else {
-  [fileOrDirName, contentTypeMime, remoteFileOrDirKey] = args;
-}
-
-if (!fs.existsSync(fileOrDirName)) {
-  setFailed(`File or directory ${fileOrDirName} does not exist`);
-  process.exit();
-}
-
-async function* walk(dir: string): AsyncGenerator<string> {
-  for await (const d of await fs.promises.opendir(dir)) {
-    const fileName = path.join(dir, d.name);
-    if (d.isDirectory()) yield* walk(fileName);
-    else if (d.isFile()) yield fileName;
-  }
-}
-
-if (fs.statSync(fileOrDirName).isFile()) {
-  await uploadFile(fileOrDirName, contentTypeMime, remoteFileOrDirKey);
-} else {
-  for await (const fileName of walk(fileOrDirName)) {
-    const remoteFileKey = `${remoteFileOrDirKey}/${fileName.replace(fileOrDirName, '')}`;
-    await uploadFile(fileName, null, remoteFileKey);
-  }
+switch (args[0]) {
+  case 'dir':
+    if (args.length !== 3) {
+      setFailed('Invalid number of arguments');
+      process.exit();
+    }
+    await uploadDir(args[1], args[2]);
+    break;
+  case 'file':
+    if (args.length !== 3 && args.length !== 4) {
+      setFailed('Invalid number of arguments');
+      process.exit();
+    }
+    await uploadFile(args[1], args[2], args[3] ?? null);
+    break;
+  case 'stdin':
+    if (args.length !== 2 && args.length !== 3) {
+      setFailed('Invalid number of arguments');
+      process.exit();
+    }
+    await uploadStdin(args[1], args[2] ?? null);
+    break;
+  default:
+    setFailed('Invalid argument');
+    process.exit();
 }
