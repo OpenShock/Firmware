bacac0aa6fb5f2b6966c236209041523c8631b1d
HentaiHeavenVR <github@heavenvr.tech>
2024-01-18 17:31:40 +0100

    LED code rework (#186)
    * Remove arduino dependencies, make code safer
    
    * Add more error checking, remove preprocessors

--- include/PinPatternManager.h
+++ include/PinPatternManager.h
@@ -2,27 +2,33 @@
 
 #include "Common.h"
 
+#include <hal/gpio_types.h>
+
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
 #include <freertos/task.h>
 
-#include <array>
 #include <cstdint>
+#include <vector>
 
 namespace OpenShock {
   class PinPatternManager {
     DISABLE_COPY(PinPatternManager);
-  public:
-    PinPatternManager(std::uint8_t gpioPin);
-    ~PinPatternManager();
 
+  public:
     struct State {
       bool level;
       std::uint32_t duration;
     };
 
+    PinPatternManager() = delete;
+    PinPatternManager(gpio_num_t gpioPin);
+    ~PinPatternManager();
+
+    bool IsValid() const { return m_gpioPin != GPIO_NUM_NC; }
+
     void SetPattern(const State* pattern, std::size_t patternLength);
-    template <std::size_t N>
+    template<std::size_t N>
     inline void SetPattern(const State (&pattern)[N]) {
       SetPattern(pattern, N);
     }
@@ -32,9 +38,8 @@ namespace OpenShock {
     void ClearPatternInternal();
     static void RunPattern(void* arg);
 
-    std::uint8_t m_gpioPin;
-    State* m_pattern;
-    std::size_t m_patternLength;
+    gpio_num_t m_gpioPin;
+    std::vector<State> m_pattern;
     TaskHandle_t m_taskHandle;
     SemaphoreHandle_t m_taskMutex;
   };
