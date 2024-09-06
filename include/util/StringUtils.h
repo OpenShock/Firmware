7f5d021885c84485d626fcd409540229ad0e1431
hhvrc <github@heavenvr.tech>
2024-09-06 11:57:57 +0200

    Move non-constexpr functions out of header

--- include/util/StringUtils.h
+++ include/util/StringUtils.h
@@ -11,67 +11,6 @@
 namespace OpenShock {
   bool FormatToString(std::string& out, const char* format, ...);
 
-  inline std::vector<std::string_view> StringSplit(const std::string_view view, char delimiter, std::size_t maxSplits = std::numeric_limits<std::size_t>::max()) {
-    if (view.empty()) {
-      return {};
-    }
-
-    std::vector<std::string_view> result = {};
-
-    std::size_t pos    = 0;
-    std::size_t splits = 0;
-    while (pos < view.size() && splits < maxSplits) {
-      std::size_t nextPos = view.find(delimiter, pos);
-      if (nextPos == std::string_view::npos) {
-        nextPos = view.size();
-      }
-
-      result.push_back(view.substr(pos, nextPos - pos));
-      pos = nextPos + 1;
-      ++splits;
-    }
-
-    if (pos < view.size()) {
-      result.push_back(view.substr(pos));
-    }
-
-    return result;
-  }
-  inline std::vector<std::string_view> StringSplit(const std::string_view view, bool (*predicate)(char delimiter), std::size_t maxSplits = std::numeric_limits<std::size_t>::max()) {
-    if (view.empty()) {
-      return {};
-    }
-
-    std::vector<std::string_view> result = {};
-
-    const char* start = nullptr;
-    for (const char* ptr = view.begin(); ptr < view.end(); ++ptr) {
-      if (predicate(*ptr)) {
-        if (start != nullptr) {
-          result.emplace_back(std::string_view(start, ptr - start));
-          start = nullptr;
-        }
-      } else if (start == nullptr) {
-        start = ptr;
-      }
-    }
-
-    if (start != nullptr) {
-      result.emplace_back(std::string_view(start, view.end() - start));
-    }
-
-    return result;
-  }
-  inline std::vector<std::string_view> StringSplitNewLines(const std::string_view view, std::size_t maxSplits = std::numeric_limits<std::size_t>::max()) {
-    return StringSplit(
-      view, [](char c) { return c == '\r' || c == '\n'; }, maxSplits
-    );
-  }
-  inline std::vector<std::string_view> StringSplitWhiteSpace(const std::string_view view, std::size_t maxSplits = std::numeric_limits<std::size_t>::max()) {
-    return StringSplit(
-      view, [](char c) { return isspace(c) != 0; }, maxSplits
-    );
-  }
   constexpr std::string_view StringTrimLeft(std::string_view view) {
     if (view.empty()) {
       return view;
@@ -102,7 +41,9 @@ namespace OpenShock {
   constexpr bool StringStartsWith(std::string_view view, std::string_view prefix) {
     return view.size() >= prefix.size() && view.substr(0, prefix.size()) == prefix;
   }
-  inline String StringToArduinoString(std::string_view view) {
-    return String(view.data(), view.size());
-  }
+  std::vector<std::string_view> StringSplit(const std::string_view view, char delimiter, std::size_t maxSplits = std::numeric_limits<std::size_t>::max());
+  std::vector<std::string_view> StringSplit(const std::string_view view, bool (*predicate)(char delimiter), std::size_t maxSplits = std::numeric_limits<std::size_t>::max());
+  std::vector<std::string_view> StringSplitNewLines(const std::string_view view, std::size_t maxSplits = std::numeric_limits<std::size_t>::max());
+  std::vector<std::string_view> StringSplitWhiteSpace(const std::string_view view, std::size_t maxSplits = std::numeric_limits<std::size_t>::max());
+  String StringToArduinoString(std::string_view view);
 }  // namespace OpenShock
