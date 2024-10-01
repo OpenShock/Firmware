a94687709e649c58761f94a7516e2abbfa518264
hhvrc <github@heavenvr.tech>
2024-10-01 11:18:34 +0200

    Undo formatting changes in Common.h

--- include/Common.h
+++ include/Common.h
@@ -3,11 +3,11 @@
 #include <cstdint>
 #include <string_view>
 
-#define DISABLE_COPY(TypeName)              \
-  TypeName(const TypeName&)       = delete; \
+#define DISABLE_COPY(TypeName)             \
+  TypeName(const TypeName&) = delete;      \
   void operator=(const TypeName&) = delete
-#define DISABLE_MOVE(TypeName)         \
-  TypeName(TypeName&&)       = delete; \
+#define DISABLE_MOVE(TypeName)             \
+  TypeName(TypeName&&) = delete;           \
   void operator=(TypeName&&) = delete
 
 #ifndef OPENSHOCK_API_DOMAIN
@@ -40,8 +40,7 @@
 
 // Check if Arduino.h exists, if not instruct the developer to remove "arduino-esp32" from the useragent and replace it with "ESP-IDF", after which the developer may remove this warning.
 #if defined(__has_include) && !__has_include("Arduino.h")
-#warning \
-  "Let it be known that Arduino hath finally been cast aside in favor of the noble ESP-IDF! I beseech thee, kind sir or madam, wouldst thou kindly partake in the honors of expunging 'arduino-esp32' from yonder useragent aloft, and in its stead, bestow the illustrious 'ESP-IDF'?"
+#warning "Let it be known that Arduino hath finally been cast aside in favor of the noble ESP-IDF! I beseech thee, kind sir or madam, wouldst thou kindly partake in the honors of expunging 'arduino-esp32' from yonder useragent aloft, and in its stead, bestow the illustrious 'ESP-IDF'?"
 #endif
 
 #if __cplusplus >= 202'302L
