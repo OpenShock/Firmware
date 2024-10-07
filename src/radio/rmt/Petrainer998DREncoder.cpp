0d11ed70225d18569eeb0bfc7f55303cc767e2b8
HeavenVR <github@heavenvr.tech>
2024-10-08 00:51:58 +0200

    Rationalize 998DR logic (#296)
    * Try to make sense of 998DR encoder

--- src/radio/rmt/Petrainer998DREncoder.cpp
+++ src/radio/rmt/Petrainer998DREncoder.cpp
@@ -5,55 +5,52 @@
 const rmt_data_t kRmtPreamble  = {1500, 1, 750, 0};
 const rmt_data_t kRmtOne       = {750, 1, 250, 0};
 const rmt_data_t kRmtZero      = {250, 1, 750, 0};
-const rmt_data_t kRmtPostamble = {1500, 0, 1500, 0}; // Some subvariants expect a quiet period between commands
+const rmt_data_t kRmtPostamble = {1500, 0, 1500, 0};  // Some subvariants expect a quiet period between commands
 
 using namespace OpenShock;
 
-std::vector<rmt_data_t> Rmt::Petrainer998DREncoder::GetSequence(uint16_t shockerId, ShockerCommandType type, uint8_t intensity) {
+std::vector<rmt_data_t> Rmt::Petrainer998DREncoder::GetSequence(uint16_t shockerId, ShockerCommandType type, uint8_t intensity)
+{
   // Intensity must be between 0 and 100
   intensity = std::min(intensity, static_cast<uint8_t>(100));
 
-  uint8_t typeVal = 0;
-  // typeInvert has the value of typeVal but bits are reversed and inverted
-  uint8_t typeInvert = 0;
+  int typeShift = 0;
   switch (type) {
-  case ShockerCommandType::Shock:
-    typeVal    = 0b0001;
-    typeInvert = 0b0111;
-    break;
-  case ShockerCommandType::Vibrate:
-    typeVal    = 0b0010;
-    typeInvert = 0b1011;
-    break;
-  case ShockerCommandType::Sound:
-    typeVal    = 0b0100;
-    typeInvert = 0b1101;
-    break;
-  // case ShockerCommandType::Light:
-  //   typeVal    = 0b1000;
-  //   typeInvert = 0b1110;
-  //   break;
-  default:
-    return {}; // Invalid type
+    case ShockerCommandType::Shock:
+      typeShift = 0;
+      break;
+    case ShockerCommandType::Vibrate:
+      typeShift = 1;
+      break;
+    case ShockerCommandType::Sound:
+      typeShift = 2;
+      break;
+    // case ShockerCommandType::Light:
+    //   nShift = 3;
+    //   break;
+    default:
+      return {};  // Invalid type
   }
 
-  // TODO: Channel argument?
-  // Can be [000] or [111], 3 bits wide
-  uint8_t channel = 0b000;
-  uint8_t channelInvert = 0b111;
+  uint8_t typeVal    = 0b0001 << typeShift;
+  uint8_t typeInvert = ~(0b1000 >> typeShift);
 
-  // Payload layout: [channel:3][typeVal:4][shockerID:17][intensity:7][typeInvert:4][channelInvert:3]
-  uint64_t data = (static_cast<uint64_t>(channel & 0b111) << 35 | static_cast<uint64_t>(typeVal & 0b1111) << 31 | static_cast<uint64_t>(shockerId & 0x1FFFF) << 14 | static_cast<uint64_t>(intensity & 0x7F) << 7 | static_cast<uint64_t>(typeInvert & 0b1111) << 3 | static_cast<uint64_t>(channelInvert & 0b111));
+  // TODO: Channel argument?
+  uint8_t channel       = 0b1000;  // Can be [1000] or [1111], 4 bits wide
+  uint8_t channelInvert = 0b1110;  // Can be [1110] or [0000], 4 bits wide
+
+  // TODO: Below ShockerID is 17 bits wide, and intensity is 7 bits wide. This is weird as ShockerID has 1 more bit and intensity has 1 less bit than 8 bit alignment. This needs investigation.
+  // Payload layout: [channel:4][typeVal:4][shockerID:17][intensity:7][typeInvert:4][channelInvert:4] (40 bits)
+  uint64_t data
+    = (static_cast<uint64_t>(channel & 0b1111) << 36 | static_cast<uint64_t>(typeVal & 0b1111) << 32 | static_cast<uint64_t>(shockerId & 0x1FFFF) << 15 | static_cast<uint64_t>(intensity & 0x7F) << 8 | static_cast<uint64_t>(typeInvert & 0b1111) << 4 | static_cast<uint64_t>(channelInvert & 0b1111));
 
   std::vector<rmt_data_t> pulses;
   pulses.reserve(43);
 
   // Generate the sequence
   pulses.push_back(kRmtPreamble);
-  pulses.push_back(kRmtOne);
-  Internal::EncodeBits<38>(pulses, data, kRmtOne, kRmtZero);
-  pulses.push_back(kRmtZero);
-  pulses.push_back(kRmtZero);
+  Internal::EncodeBits<40>(pulses, data, kRmtOne, kRmtZero);
+  pulses.push_back(kRmtZero);  // Idk why this is here, the decoded protocol has it
   pulses.push_back(kRmtPostamble);
 
   return pulses;
