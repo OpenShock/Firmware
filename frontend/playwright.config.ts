b26899b5533782692bd7782b9b9f3fe612393159
hhvrc <github@heavenvr.tech>
2024-10-21 11:54:32 +0200

    Fix tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -3,10 +3,10 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'pnpm run build && pnpm run preview',
-    port: 4173
+    port: 4173,
   },
-  testDir: 'e2e',
-  testMatch: /(.+\.)?(test|spec)\.[jt]s/
+  testDir: 'tests',
+  testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 };
 
 export default config;
