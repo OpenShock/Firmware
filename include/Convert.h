8d6f80581aae1c382a9529cbd88b64b5316545b5
hhvrc <github@heavenvr.tech>
2024-10-24 01:06:42 +0200

    Add integer to string methods

--- include/Convert.h
+++ include/Convert.h
@@ -3,9 +3,21 @@
 #include <hal/gpio_types.h>
 
 #include <cstdint>
+#include <string>
 #include <string_view>
 
 namespace OpenShock::Convert {
+  void FromInt8(int8_t val, std::string& str);
+  void FromUint8(uint8_t val, std::string& str);
+  void FromInt16(int16_t val, std::string& str);
+  void FromUint16(uint16_t val, std::string& str);
+  void FromInt32(int32_t val, std::string& str);
+  void FromUint32(uint32_t val, std::string& str);
+  void FromInt64(int64_t val, std::string& str);
+  void FromUint64(uint64_t val, std::string& str);
+  void FromBool(bool val, std::string& str);
+  void FromGpioNum(gpio_num_t val, std::string& str);
+
   bool ToInt8(std::string_view str, int8_t& val);
   bool ToUint8(std::string_view str, uint8_t& val);
   bool ToInt16(std::string_view str, int16_t& val);
