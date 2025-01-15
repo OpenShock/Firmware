c1fe72b408bd39f62990606e4e89470288903def
hhvrc <github@heavenvr.tech>
2025-01-15 16:22:09 +0100

    Further minify frontend build artifacts

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
     output: {
-      bundleStrategy: 'single',
+      bundleStrategy: 'inline',
     },
   },
 };
