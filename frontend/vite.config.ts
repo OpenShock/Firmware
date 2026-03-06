fcc452e5306b5d947d05f773e707ac0a1331735a
HeavenVR <github@heavenvr.tech>
2026-03-06 09:57:25 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig, type UserConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
-
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
-} as UserConfig); // TODO: "test" is not a valid property of the defineconfig argument? This needs to get fixed
+});
