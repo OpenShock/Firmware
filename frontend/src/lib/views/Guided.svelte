673d6ae590cb4b28577a29177ae2308e7a02832c
HeavenVR <github@heavenvr.tech>
2026-03-23 23:48:48 +0100

    Add shocker test step

--- frontend/src/lib/views/Guided.svelte
+++ frontend/src/lib/views/Guided.svelte
@@ -13,6 +13,7 @@
   } from '$lib/components/ui/stepper';
   import PinsStep from '$lib/components/steps/HardwareStep.svelte';
   import WiFiStep from '$lib/components/steps/WiFiStep.svelte';
+  import TestStep from '$lib/components/steps/TestStep.svelte';
   import AccountStep from '$lib/components/steps/AccountStep.svelte';
   import { hubState } from '$lib/stores';
   import { Button } from '$lib/components/ui/button';
@@ -28,19 +29,22 @@
   let isDIY = $derived(!hubState.hasPredefinedPins);
   let wifiConnected = $derived(hubState.wifiConnectedBSSID !== null);
   let pinConfigured = $derived(hubState.config?.rf?.txPin != null);
-  let totalSteps = $derived(isDIY ? 3 : 2);
 
-  let canAdvance = $derived(
-    isDIY
-      ? currentStep === 1
-        ? pinConfigured
-        : currentStep === 2
-          ? wifiConnected
-          : false
-      : currentStep === 1
-        ? wifiConnected
-        : false
-  );
+  // Steps: DIY = Pins, Test, WiFi, Account (4)  |  Prebuilt = Test, WiFi, Account (3)
+  let totalSteps = $derived(isDIY ? 4 : 3);
+
+  // Step number mapping
+  let testStep = $derived(isDIY ? 2 : 1);
+  let wifiStep = $derived(isDIY ? 3 : 2);
+  let accountStep = $derived(isDIY ? 4 : 3);
+
+  let canAdvance = $derived.by(() => {
+    if (isDIY && currentStep === 1) return pinConfigured;
+    // Test step: always advanceable (testing is optional)
+    if (currentStep === testStep) return true;
+    if (currentStep === wifiStep) return wifiConnected;
+    return false;
+  });
 
   let isLastStep = $derived(currentStep === totalSteps);
   let canFinish = $derived(wifiConnected && hubState.accountLinked);
@@ -63,7 +67,18 @@
           </StepperItem>
         {/if}
 
-        <StepperItem step={isDIY ? 2 : 1}>
+        <StepperItem step={testStep}>
+          <StepperTrigger>
+            <StepperIndicator />
+            <div class="hidden sm:block">
+              <StepperTitle>Test</StepperTitle>
+              <StepperDescription>Verify shocker</StepperDescription>
+            </div>
+          </StepperTrigger>
+          <StepperSeparator />
+        </StepperItem>
+
+        <StepperItem step={wifiStep}>
           <StepperTrigger>
             <StepperIndicator />
             <div class="hidden sm:block">
@@ -74,7 +89,7 @@
           <StepperSeparator />
         </StepperItem>
 
-        <StepperItem step={isDIY ? 3 : 2}>
+        <StepperItem step={accountStep}>
           <StepperTrigger>
             <StepperIndicator />
             <div class="hidden sm:block">
@@ -86,15 +101,11 @@
       </StepperNav>
 
       <div class="flex-1 rounded-lg border p-4">
-        {#if isDIY}
-          {#if currentStep === 1}
-            <PinsStep />
-          {:else if currentStep === 2}
-            <WiFiStep />
-          {:else}
-            <AccountStep />
-          {/if}
-        {:else if currentStep === 1}
+        {#if isDIY && currentStep === 1}
+          <PinsStep />
+        {:else if currentStep === testStep}
+          <TestStep />
+        {:else if currentStep === wifiStep}
           <WiFiStep />
         {:else}
           <AccountStep />
@@ -111,7 +122,7 @@
         {:else}
           <StepperNext disabled={!canAdvance}>
             {#if !canAdvance}
-              {isDIY && currentStep === 1 ? 'Configure pins first' : 'Connect WiFi first'}
+              {isDIY && currentStep === 1 ? 'Configure pins first' : currentStep === wifiStep ? 'Connect WiFi first' : ''}
             {:else}
               Next
             {/if}
